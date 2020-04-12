9ec32cc48595c6baac92e50d689da4d369589e5b
Ahmed Abdelwadod <me@ahmedwadod.com>
2020-04-12 20:30:12 +0200

    Changed default from Shell to Bash
    Shell turns out to have some issues

--- vars.go
+++ vars.go
@@ -11,7 +11,7 @@ import (
 var (
 	flagListenAddr  = flag.String("listen-addr", "0.0.0.0:22022", "the ssh listening address")
 	flagHostKeyFile = flag.String("host-key", path.Join(os.Getenv("HOME"), ".ssh/id_rsa"), "the host key path for persistence")
-	flagEntryPoint  = flag.String("entrypoint", "/bin/sh", "the default command to execute")
+	flagEntryPoint  = flag.String("entrypoint", "/bin/bash", "the default command to execute")
 	flagRedis       = flag.String("redis", "redis://localhost:6379/0", "the redis dsn (redis://user:password@host:port/db)")
 )
 
